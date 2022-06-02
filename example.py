9f5f38768da999aed0e86f2520612771ae7edafe
r-tucci <105505165+r-tucci@users.noreply.github.com>
2022-06-02 00:26:36 -0700

    Create example.py

---
+++ example.py
@@ -0,0 +1,13 @@
+def add(a, b):
+  return a + b
+
+def test_add():
+  assert add(2,3) == 5
+  assert add('space', 'ship') == 'spaceship'
+  
+def substract(a, b):
+  return a + b # this will be fixed
+
+# This will be implemented later
+#def test_substract():
+# assert substract(2,3) == -1
